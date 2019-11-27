cfb0ca35436326497e0c08a3e8446105603e2dfa
divalentSilver <argentumtooth@gmail.com>
2019-11-27 20:50:52 +0000

    Wrote unit tests for Location

--- tests/uk/ac/ed/bikerental/TestLocation.java
+++ tests/uk/ac/ed/bikerental/TestLocation.java
@@ -4,10 +4,23 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 
 class TestLocation {
+	private Location loc1, loc2, loc3;
+	
     @BeforeEach
     void setUp() throws Exception {
-        // TODO: setup some resources before each test
+        this.loc1 = new Location("EH8 9RD", "100 Meow St");
+        this.loc2 = new Location("EH8 9RE", "104 Meow St");
+        this.loc3 = new Location("ED8 4GE", "104 Meow St");
     }
     
-    // TODO: put some tests here
+    // Sample JUnit tests checking toYears works
+    @Test
+    void testIsNearToTrue() {
+    	assertTrue(loc1.isNearTo(loc2));
+    }
+    
+    @Test
+    void testIsNearToFalse() {
+    	assertFalse(loc1.isNearTo(loc3));
+    }
 }
